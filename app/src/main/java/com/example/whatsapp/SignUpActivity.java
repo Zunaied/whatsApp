17cc6c19b42fda8484af6317b58b93c79eac4384
Rafee <zunaiedrafee@gmail.com>
2021-10-02 18:40:28 +0600

    whatsApp

---
+++ app/src/main/java/com/example/whatsapp/SignUpActivity.java
@@ -0,0 +1,15 @@
+package com.example.whatsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        getSupportActionBar().hide();//to hide toolbar
+    }
+}
